e85aaee7076a68ec6d53dae4342011d735eca613
Jae Jang <jaejang5566@gmail.com>
2020-12-30 23:25:55 -0800

    initial setup

---
+++ jwt-token/src/controllers/user.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+
+const NAMESPACE = "Users";
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    
+};
+const register = (req: Request, res: Response, next: NextFunction) => {
+    
+};
+const login = (req: Request, res: Response, next: NextFunction) => {
+    
+};
+const getAllusers = (req: Request, res: Response, next: NextFunction) => {
+    
+};
+
+export default {  };
